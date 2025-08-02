facdd0413ae99adc16513e1715f4b51a48e23a61
Volvick <volvick5050@gmail.com>
2025-08-02 15:30:52 +0530

    heading correct

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => (
       <img src="/public/assets/curve (1).png" alt="Logo" />
     </div>
     <div className="header-text">
-      <h1>Tailwind Components</h1>
+      <h1>Komponi - Tailwind Components</h1>
       <p>Build cleaner UIs with production-ready Tailwind components.</p>
     </div>
   </header>
